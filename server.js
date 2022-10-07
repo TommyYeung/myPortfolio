dc9791005af65b63de61fcd49047246a7f70e576
tommyyeung <tommyyeungxd@gmail.com>
2022-10-06 21:35:42 -0400

    change in procfile

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/app'); //call express > route > contorl > 
 var app = express();
 var HOST = 'localhost';
-var port = process.env.PORT||3000; //"process.env.PORT" add for Heroku
+var port = 3000 || process.env.PORT; //"process.env.PORT" add for Heroku
 
 //set port and run
 app.listen(port);
